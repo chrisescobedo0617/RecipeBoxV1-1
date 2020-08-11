d2b77887c247ee60e9af89d2f04d1d00235e805c
unknown <jrhuebottom676@gmail.com>
2020-08-11 07:46:48 -0400

    added models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField()
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField()
+    time_required = models.CharField()
+    instructions = models.TextField()
+
+    def __str__(self):
+        return self.title
